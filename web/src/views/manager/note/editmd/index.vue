7208a1e56017e31c6feae722b2de2a70f3a827ac
王子恒 <2322978154@qq.com>
2025-04-13 22:54:40 +0800

    新增markdown文档编辑器

---
+++ web/src/views/manager/note/editmd/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <MdEditor v-model="text" :toolbars="toolbars"  >
+    <template #defToolbars >
+      <ExportPDF :modelValue="text" @onProgress="handleProgress" />
+    </template>
+  </MdEditor>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+import { ExportPDF } from '@vavt/v3-extension';
+import '@vavt/v3-extension/lib/asset/ExportPDF.css';
+import { toolbars } from "./data.js";
+
+
+const handleProgress = (progress) => {
+  console.log(`Export progress: ${progress.ratio * 100}%`);
+};
+
+const text = ref('Hello Editor!');
+
+
+</script>
